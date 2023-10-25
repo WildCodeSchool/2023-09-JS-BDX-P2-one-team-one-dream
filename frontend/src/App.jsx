25122c66655986c99f56c04bedd7419074b83444
Mathieu Chauveau <chauveau.math@gmail.com>
2023-10-25 10:32:50 +0200

    point responsive ok2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import Points from "./components/Points";
 import "./App.css";
-import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <div>
       <Points />
-      <AboutUs />
     </div>
   );
 }
