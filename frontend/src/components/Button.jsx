7e82c8868a2bf3a092e3b8c424f60e80eda78e1d
Adam HEMAMOU <h-mamou@hotmail.fr>
2023-11-02 15:44:50 +0100

    Create questionList Quiz

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Button.scss";
+import Quiz from "./Quiz";
 
 function Button() {
+  const [isOpenPopup, setIsOpenPopup] = useState(false);
+
   return (
-    <button type="button" className="buttonQuiz">
-      Calcule ton CO2
-    </button>
+    <>
+      <button
+        type="button"
+        className="buttonQuiz"
+        onClick={() => setIsOpenPopup(true)}
+      >
+        Calcule ton CO2
+      </button>
+      {isOpenPopup && <Quiz setIsOpenPopup={setIsOpenPopup} />}
+    </>
   );
 }
 
